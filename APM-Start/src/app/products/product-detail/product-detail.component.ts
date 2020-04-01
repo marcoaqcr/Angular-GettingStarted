87ffb3fb933505cfc545776844e31f5b2543d7ba
marco <marcoaqcr@gmail.com>
2020-04-01 21:33:08 +0100

    Modules module

--- APM-Start/src/app/products/product-detail/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'pm-product-detail',
@@ -10,9 +11,24 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Details';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+    this.product = {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2019",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
